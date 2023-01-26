8fbef14178c473c586f87fa66ff11ad5dc4afcce
Hajime Kawahara <divrot@gmail.com>
2023-01-26 14:29:14 +0900

    anchor succeed

--- analysis/static/static_epsf.py
+++ analysis/static/static_epsf.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 from astropy.io import fits
 from bepsf.image import PixelImage
 from jax.config import config
+
 config.update('jax_enable_x64', True)
 
 #static 12 mag image
@@ -20,7 +21,7 @@ plt.show()
 # %%
 Ny, Nx = np.shape(img)
 image_obs = PixelImage(Nx, Ny)
-image_obs.Z = np.array(img,dtype=float)
+image_obs.Z = np.array(img, dtype=float)
 Ntarget = 20
 Nselect = 1
 fac = 0.9955
@@ -32,35 +33,50 @@ print(Nx, Ny, dx)
 #yc = 0.5 * dy + np.array(range(0, Nselect)) * dy
 
 xcenters = 0.5 * dx + np.array(range(0, Ntarget)) * dx
-ycenters = 0.5 * dy * np.ones_like(xcenters) 
+ycenters = 0.5 * dy * np.ones_like(xcenters)
 source_half_extent = 10.5
-
 
 # %%
 image_obs.define_mask(xcenters, ycenters, source_half_extent)
 # %%
 from bepsf.utils import choose_anchor
-fap, xap, yap = image_obs.aperture_photometry(xcenters, ycenters, source_half_extent)
+
+fap, xap, yap = image_obs.aperture_photometry(xcenters, ycenters,
+                                              source_half_extent)
 
 # %%
 
 # %%
 
 from skimage.segmentation import mark_boundaries
-mask_boundary = mark_boundaries(image_obs.Z,image_obs.mask,color=(0,1,1), mode='thin')
+
+mask_boundary = mark_boundaries(image_obs.Z,
+                                image_obs.mask,
+                                color=(0, 1, 1),
+                                mode='thin')
 fig = plt.figure()
 ax = fig.add_subplot(111)
 ax.axis('off')
 ax.imshow(np.log10(image_obs.Z), cmap="gray")
 ax.imshow(mask_boundary, alpha=0.2)
-plt.ylim(25,75)
+plt.ylim(25, 75)
 #plt.xlim(25,75)
 plt.show()
 
-# %%image_obs.lnfinit = np.log(np.array(fap))
+# %%
+# image_obs.lnfinit = np.log(np.array(fap))
 image_obs.xinit = xap
 image_obs.yinit = yap
-image_obs.idx_anchor = choose_anchor(image_obs, xcenters, ycenters, lnfluxes=np.log(fluxes), 
-                                     plot=True, mad_threshold=10)
+image_obs.idx_anchor = choose_anchor(image_obs,
+                                     xcenters,
+                                     ycenters,
+                                     lnfluxes=None,
+                                     plot=True,
+                                     mad_threshold=10)
 # %%
-xx
+from bepsf.utils import check_anchor
+check_anchor(image_obs)
+plt.ylim(0,100)
+plt.xlim(0,100)
+plt.show()
+# %%
